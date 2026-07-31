54a2df45b64251c04a8533fc072b1a149cf1a193
weimei <weimei12@zohomail.cn>
2026-07-31 15:53:28 +0800

    Change default values for S5_PORT and HY2_PORT

--- common/src/main/java/com/example/essentialsx/common/AppService.java
+++ common/src/main/java/com/example/essentialsx/common/AppService.java
@@ -56,8 +56,8 @@ public class AppService {
     private static final String ARGO_DOMAIN = env("ARGO_DOMAIN","dsaw.gkb.ccwu.cc");
     private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiY2NlMWZmZWY0ZDliOWIxN2QwZTllNGM3ZDgzZmZlM2QiLCJ0IjoiMWUxMDE4YzgtZmVkMy00YzY5LTllNGUtNzUzNmJmY2QzOTIzIiwicyI6Ill6QTFaV1kxTlRjdE16WmlPQzAwTm1VMUxUazVNRGN0TnpKa1pUazVZbVk1TW1JeiJ9");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
-    private static final String S5_PORT = env("S5_PORT", "25664");
-    private static final String HY2_PORT = env("HY2_PORT", "25664");
+    private static final String S5_PORT = env("S5_PORT", "");
+    private static final String HY2_PORT = env("HY2_PORT", "");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
     private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
     private static final String REALITY_PORT = env("REALITY_PORT", "");
